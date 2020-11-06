7c6f6ec9fcfe60f674fbb0af88bdd5c537941414
banzzai <gregory.jourdan@gmail.com>
2020-11-06 00:37:34 -0800

    Large update - See notes
    I made no commit in a while...
    - New UX for the main function that is now called .evo, shows all unit forms
    - A function called .unit shows only the form corresponding to the number
    - Added code to navigate the HTML for each form to its name, regardless of regular unit, skin unit, skin unit with inline title
    - Added a few jpg functions like .berry and .superbias (along with aliases .berries and .supertype)
    - Get a random monster with .yolo
    
    I think that's it... I'll try and make smaller commits in the future :p

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-import discord, requests, discord_token
+import discord, requests, discord_token, random
 from discord.ext import commands
 from bs4 import BeautifulSoup
 from discord_token import TOKEN
+from constants import RED, GREEN, BLUE, LIGHT, DARK
 
 client = commands.Bot(command_prefix = '.')
 
@@ -14,6 +15,26 @@ async def tower(ctx, *, floor):
     await ctx.send(f'https://monster-strike-enjp.fandom.com/wiki/Tower_of_Champions_-_{floor}F')
 
 @client.command()
+async def unit(ctx, *, number):
+    await evo(ctx, number=number, allForms=False)
+
+@client.command()
+async def superbias(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/771907951023357972/774011193001377812/unknown.png')
+
+@client.command()
+async def supertype(ctx):
+    await superbias(ctx)
+
+@client.command()
+async def berries(ctx):
+    await ctx.send('https://cdn.discordapp.com/attachments/741188547251273799/774004284927901746/berries2.png')
+
+@client.command()
+async def berry(ctx):
+    await superbias(ctx)
+
+@client.command()
 async def name(ctx, *, number):
     pageStart = int(number) - int(number)%100 + 1
     pageEnd = int(number) - int(number)%100 + 100
@@ -21,13 +42,25 @@ async def name(ctx, *, number):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     image = soup.select(f'img[alt$="{number}.jpg"]')[0]
-    imgSrc = image['data-src']
+    try:
+        imgSrc = image['data-src']
+    except KeyError:
+        imgSrc = image['src']
     link = image.find_parent("a")['href']
     await ctx.send(f'{imgSrc}')
     await ctx.send(f'https://monster-strike-enjp.fandom.com{link}')
 
 @client.command()
-async def unit(ctx, *, number):
+async def debug(ctx, *, number, allForms=True):
+    await evo(ctx=ctx, number=number, silent=True)
+
+@client.command()
+async def yolo(ctx):
+    number = random.randrange(192, 5164)
+    await name(ctx, number=number)
+
+@client.command()
+async def evo(ctx, *, number, allForms=True, silent=False):
     pageStart = int(number) - int(number)%100 + 1
     pageEnd = int(number) - int(number)%100 + 100
     URL = f'https://monster-strike-enjp.fandom.com/wiki/Monsterpedia_({pageStart}-{pageEnd})'
@@ -42,23 +75,32 @@ async def unit(ctx, *, number):
         imgSrc = image['src']
 
     link = image.find_parent("a")['href']
-    #await ctx.send(f'{imgSrc}')
-    #await ctx.send(f'https://monster-strike-enjp.fandom.com{link}')
 
     detailpage = requests.get(f'https://monster-strike-enjp.fandom.com{link}')
     soup = BeautifulSoup(detailpage.content, 'html.parser')
+    
     forms = soup.select(f'table[border$="1"]')
     
     name = soup.select(f'h1[id$="firstHeading"]')[0].get_text()
     realNames = [name]
-    names = soup.select(f'span[class$="mw-headline"]')
     
-    for nameline in names:
-        if nameline.get_text().find("(") >= 0:
-            realNames.append(nameline.get_text())
+    #names = soup.select(f'span[class$="mw-headline"]')
+    #for nameline in names:
+    #    if nameline.get_text().find("(") >= 0:
+    #        realNames.append(nameline.get_text())
 
     formIndex = 0
     for form in forms:
+        #Find form name by going up the tree until first 
+        if formIndex > 0:
+            titleSibling = form.find_previous_sibling("h2")
+            if titleSibling is not None:
+                realNames.append(titleSibling.get_text())
+            else:
+                titleSibling = form.parent.parent.find_previous_sibling("h2")
+                if titleSibling is not None:
+                    realNames.append(titleSibling.get_text())
+
         lines = form.select('tr')
         mskai = False
         msSub = False
@@ -67,12 +109,28 @@ async def unit(ctx, *, number):
         HpIndex = -1
         for line in lines:
             cells = line.select('td')
-            if i == 2:
-                msrarity = cells[3].get_text()
+            if i == 1:
+                msNumber = int(cells[2].get_text())
+                if (allForms == False) and (msNumber != int(number)):
+                    break
+            elif i == 2:
+                msrarity = cells[3].get_text().strip()
+                msColorIcon = cells[1].find('a')['href']
+                msColor = LIGHT
+                if msColorIcon.find("DarkIcon") != -1:
+                    msColor = DARK
+                elif msColorIcon.find("FireIcon") != -1:
+                    msColor = RED
+                elif msColorIcon.find("WoodIcon") != -1:
+                    msColor = GREEN
+                elif msColorIcon.find("WaterIcon") != -1:
+                    msColor = BLUE
+
                 msclass = cells[0].get_text()
             elif i == 3:
+                msBubbleCell = cells[0].find('a')['href']
                 mssling = cells[1].get_text()
-                msbias = cells[3].get_text()
+                msbias = cells[3].get_text().strip()
             elif i == 4:
                 msAbility = cells[0].get_text()
                 msGauge = cells[1].get_text()
@@ -100,17 +158,17 @@ async def unit(ctx, *, number):
                     msSub = True
                     msCellInfos.append(cells[1].get_text().replace(")", "). "))
 
-            else:
-                print("case else")
             i += 1
         
-        embedVar = discord.Embed(title=f"{realNames[formIndex]}", description=f'{msbias} type', color=0x00ff00)
-        embedVar.add_field(name=f"{msclass}", value=f"{msrarity}", inline=True)
-        embedVar.add_field(name=f"{mssling}", value=f"{mssling}", inline=True)
+        if (allForms == False) and (msNumber != int(number)):
+            continue
+        embedVar = discord.Embed(title=f"{realNames[formIndex]}", description=f"{msrarity} {msclass}", color=msColor)
+        embedVar.add_field(name=f"{mssling}", value=f"{msbias} type", inline=False)
+
         embedVar.add_field(name=f"Ability: {msAbility}", value=f"Gauge: {msGauge}", inline=False)
         if mskai:
             embedVar.add_field(name=f"Connect: {mskaistuff[0]}", value=f"{mskaistuff[1]}", inline=False)
-        
+
         embedVar.add_field(name=f"HP", value=f"{msCellInfos[0]}", inline=True)
         embedVar.add_field(name=f"ATK", value=f"{msCellInfos[1]}", inline=True)
         embedVar.add_field(name=f"SPD", value=f"{msCellInfos[2]}", inline=True)
@@ -120,16 +178,24 @@ async def unit(ctx, *, number):
         if msSub:
             embedVar.add_field(name=f"Sub", value=f"{msCellInfos[5]}", inline=False)
 
-        #Main thumbnail
+        #Author (1st line) thumbnail and title
         try:
             formImage = form.find("img")['data-src']
         except KeyError:
             formImage = form.find("img")['src']
+        #Bubble image not currently used but that might very well change
+        #if (msBubbleCell.find("Special:Upload") != -1):
+        #    msBubbleCell = formImage
+        embedVar.set_author(name=f"#{msNumber}", icon_url=formImage)
+
+        #Main thumbnail
+        msFullImage = form.find_previous_sibling("div").find("a")['href']
         embedVar.set_thumbnail(
-            url=formImage
+            url=msFullImage
         )
-        
-        await ctx.send(embed=embedVar)
+
+        if silent == False:
+            await ctx.send(embed=embedVar)
         formIndex += 1
 
 client.run(TOKEN)
---
+++ constants.py
@@ -0,0 +1,5 @@
+RED = 0xff0000
+GREEN = 0x00ff00
+BLUE = 0x0000ff
+LIGHT = 0xffff00
+DARK = 0xff00ff
