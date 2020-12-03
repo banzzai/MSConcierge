359f6ec26aa52d953d76b05973b0f4ca31de076d
banzzai <gregory.jourdan@gmail.com>
2020-12-02 16:36:18 -0800

    Update bot.py
    Adding functions and checks to verify monster wiki page exists.
    
    Also a different parsin of Kai ability text, since not all version contain the "condition" keyword afterall.

--- bot.py
+++ bot.py
@@ -31,16 +31,34 @@ async def berries(ctx):
     await ctx.send('https://cdn.discordapp.com/attachments/741188547251273799/774004284927901746/berries2.png')
 
 @client.command()
+async def collab(ctx):
+    await ctx.send('https://twitter.com/EngMonst/status/1327139304204886019/photo/1')
+
+@client.command()
 async def berry(ctx):
     await superbias(ctx)
 
-async def getUnitPage(number):
+def isInRange(number, soup):
+    #This should extract a link to the last pedia page, from the top menu (second) tab (first) dropdown link
+    lastPageLink = soup.select(f'ul[class$="wds-list wds-is-linked wds-has-bolded-items"]')[1].find("a")['href']
+    lastPediaNumer = int(lastPageLink[-5:-1])
+    
+    if (int(number) > lastPediaNumer):
+        return False
+    else:
+        return True
+
+async def getUnitPage(number, JPFallback=False):
     pageStart = int(number) - int(number)%100 + 1
     pageEnd = int(number) - int(number)%100 + 100
     URL = f'https://monster-strike-enjp.fandom.com/wiki/Monsterpedia_({pageStart}-{pageEnd})'
     
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
+    
+    if not isInRange(number, soup):
+        return None
+
     image = soup.select(f'img[alt$="{number}.jpg"]')[0]
 
     link = image.find_parent("a")
@@ -48,16 +66,24 @@ async def getUnitPage(number):
     newLink = link.find_next_sibling()
     #There is a link, but it's to create the wiki entry, so no result for our purpose
     if (str(newLink).find(CREATE_WIKI_PAGE_HREF) != -1):
-        return None
-    
-    pageUrl = f'https://monster-strike-enjp.fandom.com{linkHref}'
+        pageUrl = f'https://monster-strike-enjp.fandom.com{linkHref}'
 
-    try:
-        detailpage = requests.get(pageUrl)
-    except:
-        pageUrl = None
+        try:
+            #Some links go nowhere so we're testing this one
+            detailPage = requests.get(pageUrl)
+        except:
+            detailPage = None
 
-    return pageUrl
+    #At this stage if detailPage is Empty we need to check JP wiki if desired
+    if detailPage is None and JPFallback:
+        pageUrl = f'https://monst.appbank.net/monster/{number}.html'
+        detailPage = requests.get(pageUrl)
+
+        #Currently there is one image in the 404 page and it has 404 in its name. Hopefully that's a semi-reliable criteria
+        img = detailPage.find("img")['src']
+        print(f"img in jp page {img}")
+
+    return detailPage
 
 @client.command()
 async def name(ctx, *, number, needHatcher=False):
@@ -72,10 +98,9 @@ async def debug(ctx, *, number, allForms=True):
     await evo(ctx=ctx, number=number, silent=True)
 
 async def isHatcher(number):
-    unitPage = await getUnitPage(number)
-    print(f'number {number}, page {unitPage}')
-    if (unitPage is not None):
-        detailpage = requests.get(unitPage)
+    detailpage = await getUnitPage(number)
+    
+    if (detailpage is not None):
         soup = BeautifulSoup(detailpage.content, 'html.parser')
 
         forms = soup.select(f'table[border$="1"]')
@@ -122,6 +147,11 @@ def parseObtain(obtainHTML):
         return MONSTER_DX_HATCHER
     return UNKNOWN
 
+async def noWiki(ctx, number):
+    await ctx.send("Wiki doesn't have it :(")
+    await ctx.send("Here's a link to a different database(jp)")
+    await ctx.send(f'https://monst.appbank.net/monster/{number}.html')
+
 @client.command()
 async def evo(ctx, *, number, allForms=True, silent=False):
     pageStart = int(number) - int(number)%100 + 1
@@ -130,8 +160,22 @@ async def evo(ctx, *, number, allForms=True, silent=False):
 
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-    image = soup.select(f'img[alt$="{number}.jpg"]')[0]
     
+    #Why you gotta be asking for a monster number that is higher than the most recent in the wiki?
+    if not isInRange(number, soup):
+        await noWiki(ctx, number)
+    
+    #We are looking for an image with the monster number, and it's associated href (the wiki page)
+    imageHTML = soup.select(f'img[alt$="{number}.jpg"]')
+    
+    #It's possible there is no entry at all, even though we are on the right page for this monster (ex: 214)
+    if (len(imageHTML) > 0):
+        image = imageHTML[0]
+    else:
+        #No entry for that number
+        await noWiki(ctx, number)
+        exit
+
     try:
         imgSrc = image['data-src']
     except KeyError:
@@ -142,9 +186,8 @@ async def evo(ctx, *, number, allForms=True, silent=False):
     try:
         detailpage = requests.get(f'https://monster-strike-enjp.fandom.com{link}')
     except:
-        await ctx.send("Wiki doesn't have it :(")
-        await ctx.send("Here's a link to a different database(jp)")
-        await ctx.send(f'https://monst.appbank.net/monster/{number}.html')
+        #There is nothing behind the wiki link
+        await noWiki(ctx, number)
         exit
 
     soup = BeautifulSoup(detailpage.content, 'html.parser')
@@ -208,7 +251,7 @@ async def evo(ctx, *, number, allForms=True, silent=False):
                 if len(cells) > 0:
                     mskai = True
                     name = name + " Kai"
-                    mskaistuff = cells[1].get_text().split("Condition: ")
+                    mskaistuff = cells[1].get_text('|').split("|")
             elif i > 1 and len(cells) > 0:
                 if HpIndex == -1:
                     HpIndex = i
